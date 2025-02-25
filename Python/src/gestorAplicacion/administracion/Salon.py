6bd29d7d54f5291ebd8d0c553472509a22132d26
Tomas Velasquez <tvelasqueze@unal.edu.co>
2025-02-24 22:05:16 -0500

    prueba

--- Python/src/gestorAplicacion/administracion/Salon.py
+++ Python/src/gestorAplicacion/administracion/Salon.py
@@ -1,58 +1,69 @@
 from gestorAplicacion.administracion.Horario import Horario
 
-class Salon:
 
+class Salon:
     _salones = []
 
     def __init__(self, lugar, aforo):
         self._lugar = lugar
         self._aforo = aforo
-        self._horario = Horario()
+        self._horario = Horario()  
         Salon._salones.append(self)
 
+    # METODOS
 
-        @classmethod
-        def mostrarSalones(cls):
-            retorno = ""
-            i = 0
-            for salon in Salon._salones:
-                i += 1
-                retorno = str(i) + ". " + salon._lugar + ".\n"
-            return retorno
-
-
-        @classmethod
-        def nombresSalones(cls):
-            nombres = []
-            for salon in Salon._salones:
-                nombres.append(salon.getLugar())
-            return nombres
+    @classmethod
+    def mostrarSalones(cls):
+        retorno = ""
+        i = 1
+        for salon in Salon._salones:
+            i += 1
+            retorno += str(i) + ". " + salon._lugar + ".\n"
+        return retorno
     
+    @classmethod
+    def nombresSalones(cls):
+        nombres = []
+        for salon in Salon._salones:
+            nombres.append(salon.getLugar())
+        return nombres
+    
+    @classmethod
+    def encontrarSalon(cls, salon):
+        for s in Salon._salones:
+            if s.getLugar()==salon:
+                return s
 
-        def getLugar(self):
-            return self._lugar
-        
-        def setLugar(self, lugar):
-            self._lugar = lugar
-        
-        def getAforo(self):
-            return self._aforo
-        
-        def setAforo(self, aforo):
-            self._aforo = aforo
-        
-        def getHorario(self):
-            return self._horario
-        
-        def setHorario(self, horario):
-            self._horario = horario
-        
+    # Setters y Getters
 
-        @classmethod
-        def getSalones(cls):
-            return cls._salones
-        
-        @classmethod
-        def setSalones(cls, salones):
-            cls._salones = salones
-            
+    def getLugar(self):
+        return self._lugar
+
+    def setLugar(self, lugar):
+        self._lugar = lugar
+
+    def getAforo(self):
+        return self._aforo
+
+    def setLugar(self, aforo):
+        self._aforo = aforo
+
+    def getLugar(self):
+        return self._lugar
+
+    def setLugar(self, lugar):
+        self._lugar = lugar
+
+    def getHorario(self):
+        return self._horario
+
+    def setHorario(self, horario):
+        self._horario = horario
+
+    @classmethod
+    def getSalones(cls):
+        return cls._salones
+
+    @classmethod
+    def setSalones(cls, salones):
+        cls._salones = salones
