514edf6a610b95b0a756a181c72e323025c15483
LinaSanchezM <lsanchezmo@unal.edu.co>
2025-02-24 21:27:33 -0500

    Up Materia

--- Python/src/gestorAplicacion/administracion/Materia.py
+++ Python/src/gestorAplicacion/administracion/Materia.py
@@ -61,6 +61,158 @@ class Materia:
             retorno.append(materia.getNombre())
         return retorno
 
+    @staticmethod
+    def puedeVerMateria(estudiante, grupo):
+        from gestorAplicacion.usuario.Coordinador import Coordinador
+        
+        if not (
+            estudiante.getCreditos() + grupo.getMateria().getCreditos()
+            <= Coordinador.getLimitesCreditos()
+        ):
+            return False
+        if not estudiante.getHorario().comprobarDisponibilidad(grupo.getHorario()):
+            return False
+        if grupo.getCupos() == 0:
+            return False
+        if not Materia.comprobarPrerrequisitos(estudiante, grupo.getMateria()):
+            return False
+        return True
+
+    @staticmethod
+    def comprobarPrerrequisitos(estudiante, materiap):
+
+        materiasVistas = []
+        materias_estudiante=[]
+        for materia in estudiante.getMaterias():
+            materias_estudiante.append(materia.getCodigo())
+        for grupo in estudiante.getGruposVistos():
+            materiasVistas.append(grupo.getMateria().getCodigo())
+        for materia in materiap.getPrerrequisitos():
+            flag = False
+            for vista in materiasVistas:
+                if materia.getCodigo() == vista:
+                    flag = True
+                    break
+            if not flag:
+                return False
+        if materiap.getCodigo() in materiasVistas:
+            return False
+        if materiap.getCodigo() in materias_estudiante:
+            return False
+        return True
+
+    def cantidadCupos(self):
+        cantidad = 0
+        for pGrupo in self.grupos:
+            cantidad += pGrupo.getCupos()
+        return cantidad
+
+    def crearGrupo(self, numero, profesor, horario, cupos, salon):
+        grupo = Grupo(self, numero, profesor, horario, cupos, salon)
+
+        self.cantidadCupos()
+        self.grupos.append(grupo)
+
+        return grupo
+
+    def mostrarContenidos(self):
+        contenido = (
+            "Materia: "
+            + self.nombre
+            + "\n"
+            + "Codigo: "
+            + str(self.codigo)
+            + "\n"
+            + "Creditos: "
+            + str(self.creditos)
+            + "\n"
+            + "Facultad: "
+            + self.facultad
+            + "\n"
+            + "Descripcion:\n"
+            + self.descripcion
+        )
+        return contenido
+
+    def existenciaGrupo(self, grupoBuscado):
+        if self.grupos:
+            for grupo in self.grupos:
+                if grupo == grupoBuscado:
+                    return True
+            return False
+        else:
+            return False
+
+    def eliminarGrupo(self, numero):
+        if numero>0 and numero<=len(self.grupos):
+            grupo = self.grupos[numero - 1]
+            grupo.getProfesor().desvincularGrupo(grupo)
+            grupo.getSalon().getHorario().liberarHorario(grupo.getHorario())
+            self.grupos.remove(grupo)
+            
+            for i in range(numero - 1, len(self.grupos)):
+                grupoCamb = self.grupos[i]
+                nGrupoAnt = grupoCamb.getNumero()
+                grupoCamb.setNumero(nGrupoAnt - 1)
+        else:
+            raise CampoInvalido()
+
+    def agregarGrupo(self, numero, profesor, horario, cupos, salon):
+        dispSalon = True
+        dispProfesor = True
+        daMateria = profesor.daMateria(self.nombre)
+
+        dispProfesor = profesor.getHorario().comprobarDisponibilidad(horario)
+        dispSalon = salon.getHorario().comprobarDisponibilidad(horario)
+
+        if dispProfesor and dispSalon and daMateria:
+            nGrupo = self.crearGrupo(numero, profesor, horario, cupos, salon)
+            salon.getHorario().ocuparHorario(nGrupo, horario)
+            profesor.vincularGrupo(nGrupo)
+        else:
+            raise GrupoNoAgregado()
+
+    def agregarGrupoHecho(self, grupoHecho):
+        self.grupos.append(grupoHecho)
+
+    def buscarGrupoDeEstudiante(self, estudiante):
+        for grupo in self.grupos:
+            for e in grupo.getEstudiantes():
+                if e == estudiante:
+                    return grupo
+        return None
+
+    def hacerAbreviatura(self, nombre):
+        abreviatura = ""
+        if len(nombre) <= 13:
+            self.abreviatura = nombre
+        else:
+            palabras = nombre.split()
+            for palabra in palabras:
+                if len(palabra) >= 3:
+                    abreviatura += palabra[:3] + " "
+                else:
+                    abreviatura += palabra[: len(palabra)] + " "
+            if len(abreviatura) <= 13:
+                self.abreviatura = abreviatura
+            else:
+                self.abreviatura = abreviatura[:13]
+
+    def mostrarGrupos(self):
+        retorno = ""
+        i = 1
+        for grupo in self.grupos:
+            retorno += f"{i}. {grupo.getNumero()}.\n"
+            i += 1
+        return retorno
+    
+    def calcularCupos(self):
+        cupos =0
+        for i in self.grupos:
+            cupos+=i.getCupos()
+            
+        return cupos
+
     def getNombre(self):
         return self.nombre
 
@@ -70,6 +222,48 @@ class Materia:
     def getCodigo(self):
         return self.codigo
     
+    def getDescripcion(self):
+        return self.descripcion
+
+    def setDescripcion(self, descripcion):
+        self.descripcion = descripcion
+    
+    def getCreditos(self):
+        return self.creditos
+
+    def setCreditos(self, creditos):
+        self.creditos = creditos
+
+    def getFacultad(self):
+        return self.facultad
+
+    def setFacultad(self, facultad):
+        self.facultad = facultad
+
+    def getCupos(self):
+        return self.calcularCupos()
+
+    def setCupos(self, cupos):
+        self.cupos = cupos
+
+    def getPrerrequisitos(self):
+        return self.prerrequisitos
+
+    def setPrerrequisitos(self, prerrequisitos):
+        self.prerrequisitos = prerrequisitos
+
+    def getGrupos(self):
+        return self.grupos
+
+    def setGrupos(self, grupos):
+        self.grupos = grupos
+
+    def getAbreviatura(self):
+        return self.abreviatura
+
+    def setAbreviatura(self, abreviatura):
+        self.abreviatura = abreviatura
+
     @classmethod
     def getMateriasTotales(cls):
         return cls.materiasTotales
